3772acab8b7e0e7fbf8d4b05962adb4aa847e360
efecini <efecini@gmail.com>
2025-12-29 23:43:51 +0300

    trying navbar 1

--- FE/thousandlirabitcoin/src/App.vue
+++ FE/thousandlirabitcoin/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <div>
+    <Navbar />
     <div
       id="TopNav"
       class="w-[calc(100%-0px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 flex items-center justify-between"
