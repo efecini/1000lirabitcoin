0ace864e07c0f48ec5fe29a3ba844ddd43d82e41
efecini <efecini@gmail.com>
2026-01-01 02:18:40 +0300

    add navbar and tailwind

--- FE/thousandlirabitcoin/src/App.vue
+++ FE/thousandlirabitcoin/src/App.vue
@@ -1,18 +1,61 @@
 <script setup>
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
-import ColorPicker from 'primevue/colorpicker';
-
+import Menubar from 'primevue/menubar';
 import { ref } from 'vue';
 
-const color = ref();
 const text = ref('');
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+  },
+  {
+    label: 'Features',
+    icon: 'pi pi-star',
+  },
+  {
+    label: 'Projects',
+    icon: 'pi pi-search',
+    items: [
+      {
+        label: 'Components',
+        icon: 'pi pi-bolt',
+      },
+      {
+        label: 'Blocks',
+        icon: 'pi pi-server',
+      },
+      {
+        label: 'UI Kit',
+        icon: 'pi pi-pencil',
+      },
+      {
+        label: 'Templates',
+        icon: 'pi pi-palette',
+        items: [
+          {
+            label: 'Apollo',
+            icon: 'pi pi-palette',
+          },
+          {
+            label: 'Ultima',
+            icon: 'pi pi-palette',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Contact',
+    icon: 'pi pi-envelope',
+  },
+]);
 </script>
 
 <template>
   <div>
-    <InputText v-model="text" /> <Button label="Save" />
-    {{ text }}
-    <ColorPicker v-model="color" />
+    <Menubar :model="items" />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
 </template>
