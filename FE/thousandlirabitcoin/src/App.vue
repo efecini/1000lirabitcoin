4fd2bc5df212914d313bc83e1d16231c1a174c58
efecini <efecini@gmail.com>
2026-01-02 03:34:13 +0300

    make navbar a particular component and add it as a component into app.vue

--- FE/thousandlirabitcoin/src/App.vue
+++ FE/thousandlirabitcoin/src/App.vue
@@ -1,61 +1,12 @@
 <script setup>
-import InputText from 'primevue/inputtext';
-import Button from 'primevue/button';
-import Menubar from 'primevue/menubar';
-import { ref } from 'vue';
-
-const text = ref('');
-const items = ref([
-  {
-    label: 'Home',
-    icon: 'pi pi-home',
-  },
-  {
-    label: 'Features',
-    icon: 'pi pi-star',
-  },
-  {
-    label: 'Projects',
-    icon: 'pi pi-search',
-    items: [
-      {
-        label: 'Components',
-        icon: 'pi pi-bolt',
-      },
-      {
-        label: 'Blocks',
-        icon: 'pi pi-server',
-      },
-      {
-        label: 'UI Kit',
-        icon: 'pi pi-pencil',
-      },
-      {
-        label: 'Templates',
-        icon: 'pi pi-palette',
-        items: [
-          {
-            label: 'Apollo',
-            icon: 'pi pi-palette',
-          },
-          {
-            label: 'Ultima',
-            icon: 'pi pi-palette',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: 'Contact',
-    icon: 'pi pi-envelope',
-  },
-]);
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <div>
-    <Menubar :model="items" />
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <Navbar />
+    <div class="flex items-center justify-center min-h-screen">
+      <h1 class="text-3xl font-bold underline">Chart will be here. Chart.js</h1>
+    </div>
   </div>
 </template>
